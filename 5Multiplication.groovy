34740c02fefd23fcff83c5a4cda141234eb39961
BBK-PiJ-2016-47 <h.l.spencer@lse.ac.uk>
2016-10-09 07:45:34 -0700

    Rename file, editing whitespace

--- 5Multiplication.groovy
+++ 5Multiplication.groovy
@@ -5,13 +5,15 @@ int num2 = 0
 println "Please key in a number: "
 s = System.console().readLine()
 num1 = Integer.parseInt(s)
+
 println "Please key in a second number: "
 s = System.console().readLine()
 num2 = Integer.parseInt(s)
+
 int i = 0
 int num3 = 0
 while (i < num2) {
-num3 = num3 + num1
-i++
+  num3 = num3 + num1
+  i++
 }
 println num3
