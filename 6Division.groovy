31cd8e7acb7a29b141f5f86511d7db503c5038ac
BBK-PiJ-2016-47 <h.l.spencer@lse.ac.uk>
2016-10-09 07:44:05 -0700

    Week 1 Exercises

--- 6Division.groovy
+++ 6Division.groovy
@@ -1,9 +1,11 @@
 String s=""
 int num1 = 0
 int num2 = 0
+
 println "Please type in a number: "
 s = System.console().readLine()
 num1 = Integer.parseInt(s)
+
 println "Please type in another number: "
 s = System.console().readLine()
 num2 = Integer.parseInt(s)
@@ -12,7 +14,7 @@ int i = 0
 int num3 = 0
 
 while (num3 < num1) {
-num3 = num3 + num2
-i++
+  num3 = num3 + num2
+  i++
 }
 println num1 + " divided by " + num2 + " equals " + i
