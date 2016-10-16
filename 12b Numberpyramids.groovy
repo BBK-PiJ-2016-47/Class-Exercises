07bbde4534cd8db34b92d1f0fe6af28549a10fa3
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-10-16 19:24:04 +0100

    Right alignment fixed
    Still need to add spaces to fix left alignment

--- 12b Numberpyramids.groovy
+++ 12b Numberpyramids.groovy
@@ -6,14 +6,15 @@ int x = 0
 while (x < num) {
   for (int i=0; i<=num; i++) {
     print " " * (num-i)
-    for (int j=0; j<i; j++) {
-      if (i <10) {
-       print i + " "
-      } else {
-        print i
-      }
+    if (i < 10) {
+      print " " * num 
+    } else {
+      print " " * (num-i)
     }
-    println ""
-    x++
+    for (int j=0; j<i; j++) {
+      print (i)
+    }
+  println ""
+  x++
   } 
 }
