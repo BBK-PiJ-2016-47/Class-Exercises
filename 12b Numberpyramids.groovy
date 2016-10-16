a1e69507486672c96f8fc522987fff0ad914a3cb
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-10-16 19:36:20 +0100

    Fixed alignment
    It all works!

--- 12b Numberpyramids.groovy
+++ 12b Numberpyramids.groovy
@@ -7,14 +7,17 @@ while (x < num) {
   for (int i=0; i<=num; i++) {
     print " " * (num-i)
     if (i < 10) {
-      print " " * num 
+      print " " * (num+1-i) 
     } else {
       print " " * (num-i)
     }
     for (int j=0; j<i; j++) {
-      print (i)
+      if (i < 10) {
+        print i + " "
+      } else {
+        print i
+      }
     }
   println ""
   x++
-  } 
-}
+} 
