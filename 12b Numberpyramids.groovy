a4f4f6a2172003b871d1fa66d63ea25ab0474bec
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-10-16 18:29:01 +0100

    Creater 12b Numberpyramids.groovy
    Need to fix:
    1. Keeps going into an infinite loop
    2. Don't think the spacing it prints is right

---
+++ 12b Numberpyramids.groovy
@@ -0,0 +1,15 @@
+print "Please enter a number: "
+String inpt = System.console().readLine()
+int num = Integer.parseInt(inpt)
+int x = 0
+
+while (x < num) {
+  for (int i=0; i<=num; i++) {
+print " " * (num-1)
+    for (int j=0; j<i; j++) {
+       print (num)
+    }
+    println ""
+    num++
+  } 
+}
