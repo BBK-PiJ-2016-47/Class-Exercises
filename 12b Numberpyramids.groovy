21dfdb65fab86eceb87a69163e379f48fb1fa6a0
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-10-16 18:35:08 +0100

    Fixed infinite looping
    Need to fix spacing when num > 10

--- 12b Numberpyramids.groovy
+++ 12b Numberpyramids.groovy
@@ -5,11 +5,11 @@ int x = 0
 
 while (x < num) {
   for (int i=0; i<=num; i++) {
-print " " * (num-1)
+print " " * (num-i)
     for (int j=0; j<i; j++) {
-       print (num)
+       print (i)
     }
     println ""
-    num++
+    x++
   } 
 }
