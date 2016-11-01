f1c382805e018a4134a26bcc3abea4e5b4775772
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-01 11:51:36 +0000

    Moving to Week-1 and adding question

--- Week-1/8Maximising.groovy
+++ Week-1/8Maximising.groovy
@@ -1,3 +1,7 @@
+/*8 Maximising
+Write a program that reads a (arbitrarily long) sequence of positive numbers. The sequence is ended when the users
+enters “-1”. At that point, the program must output the highest number in the sequence.*/
+
 String s = ""
 int bignum = 0
 int entry = 0
