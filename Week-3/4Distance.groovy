d354a4c80b58731bb28a428ab2c003066cf07af3
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-06 16:02:10 +0000

    Create 4Distance.groovy
    Need to add a loop to make simpler

---
+++ Week-3/4Distance.groovy
@@ -0,0 +1,66 @@
+print "Please enter the second point's x coordinate: "
+s = System.console().readLine()
+Point point2 = new Point();
+point2.x = Double.parseDouble(s)
+
+print "Please enter the second point's y coordinate: "
+s = System.console().readLine()
+point2.y = Double.parseDouble(s)
+
+
+print "Please enter the third point's x coordinate: "
+s = System.console().readLine()
+Point point3 = new Point();
+point3.x = Double.parseDouble(s)
+
+print "Please enter the third point's y coordinate: "
+s = System.console().readLine()
+point3.y = Double.parseDouble(s)
+
+
+//distance between 1 & 2
+
+double xminus = (point1.x - point2.x)
+double xminussqr = xminus**2
+
+double yminus = (point1.y - point2.y)
+double yminussqr = yminus**2
+
+double sum = (xminussqr + yminussqr)
+double distance12 = Math.sqrt(sum)
+
+//distance between 1 & 3
+
+xminus = (point1.x - point3.x)
+xminussqr = xminus**2
+
+yminus = (point1.y - point3.y)
+yminussqr = yminus**2
+
+sum = (xminussqr + yminussqr)
+double distance13 = Math.sqrt(sum)
+
+//distance between 2 & 3
+
+xminus = (point2.x - point3.x)
+xminussqr = xminus**2
+
+yminus = (point2.y - point3.y)
+yminussqr = yminus**2
+
+sum = (xminussqr + yminussqr)
+double distance23 = Math.sqrt(sum)
+
+//finding which distance is bigger
+
+if (distance12 < distance13) {
+  if (distance12 < distance23) {
+    println "Point one and point two are the closest together."
+  } else {
+    println "Point two and point three are the closest together."
+  } 
+}else {
+  if (distance13 < distance23) {
+    println "Point one and point three are the closest together."
+  }
+}
