3e073720796af03379d1ba6a0a3eaca86481a5b4
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-08 16:06:10 +0000

    Create 12.groovy

---
+++ Week-3/12.groovy
@@ -0,0 +1,60 @@
+/*12 Your change, please
+Write a program that reads the total cost of a purchase and an amount of money that 
+is paid to buy it. Your program should output the correct change specifying the amount 
+of notes (50, 20, 10, 5) and coins (2, 1, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01) needed*/
+
+double price = 0.0;
+double paid = 0.0;
+
+print "Please input the price of the item: "
+String sprice = System.console().readLine();
+price = Double.parseDouble(sprice);
+
+print "Please input the amount paid for the item: "
+String Spaid = System.console().readLine();
+paid = Double.parseDouble(paid);
+
+if(price > paid) {
+  int owed = (price - paid);
+  print "You still need to pay £" + owed + " more!"
+}
+
+double zero = 0.0;
+
+int diff = (paid - price);
+print "Your change is "
+while(diff != zero) {
+
+  if (diff > 50.00) {
+    diff = diff-50.00
+    println "£50 note"
+  } else if (diff > 20.00) {
+    diff = diff - 20.00
+    println "£20 note"
+  } else if (diff > 10.00) {
+    diff = diff-10.00
+    println "£10 note"
+  } else if (diff > 5.00) {
+    diff = diff-5.00
+    println "£5 note"
+  } else if (diff > 2.00) {
+    diff = diff-2.00
+    println "£2 coin"
+  } else if (diff > 1.00) {
+    diff = diff-1.00
+    println "£1 coin"
+  } else if (diff > 0.50) {
+    diff = diff-0.50
+    println "50p coin"
+  } else if (diff > 0.20) {
+    diff = diff-0.20
+    println "20p coin"
+  } else if (diff > 0.10) {
+    diff = diff-0.10
+    println "10p coin"
+  } else if (diff > 0.05) {
+    diff = diff-0.05
+    println "5p coin"
+  }
+
+}
