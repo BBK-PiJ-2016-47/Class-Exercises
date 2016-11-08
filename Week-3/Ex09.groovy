3af89200fadfc748c2472faf9f95b20499ec9110
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-08 15:20:17 +0000

    Create Ex09.groovy

---
+++ Week-3/Ex09.groovy
@@ -0,0 +1,19 @@
+/*9 Counting letters
+Write a program that reads some text from the user and then says how many letters 
+’e’ are there in that text.Then modify the program so that it reads the text from 
+the user and then asks for a letter. The program should
+then say how many times you can find the letter in the text.
+*/
+
+println "Please type in a word: "
+String input = System.console().readLine()
+
+int inputleng = input.length();
+int counter = 0;
+  for(int i = 0; i <= inputleng; i++) {
+    if(input.charAt(i) == "c") {
+    counter++
+  }
+}
+
+print "The character \"c\" appears " + counter + " times."
