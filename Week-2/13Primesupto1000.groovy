6b9b6482677dbeee2f8ea5be960df0e33f67dea7
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-10 19:51:23 +0000

    Changing while and for loops

--- Week-2/13Primesupto1000.groovy
+++ Week-2/13Primesupto1000.groovy
@@ -1,31 +1,29 @@
 //Print all primes up to 1000
 
-int i = 2
+int num = 0;
 println "all prime numbers up to 1000"
-
-for (int num = 0;num < 1000;num++) {
+/*
+for (num = 0;num < 1000;num++) {
   while (i < 10) {
-    if ( i == num) {
-      i++
-    } else {
-      int a = num % i
-        if (a != 0) {
-          println num
+        if ((num % i) != 0) {
+          println "Prime: " + num
           i++
         } else {
           i++
         } 
       } 
     } 
-}
+
+*/
+
 while (num < 20) {
-  for (int i=0;i < 10;i++) {
-    if (i != num) {      
-     int a = num % i
-       if (a != 0) {
-          println num
-      
-    }
+  for (int i=2;i < 10;i++) {
+       if ((num % i) != 0) {
+          println num     
+          num++
+        } else {
+          num++
+        }
   }
-num++
+
 }
