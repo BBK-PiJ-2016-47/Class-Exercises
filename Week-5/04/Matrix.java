5274f234323e63a10bd58f4e839ad088371ade88
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-12 12:25:05 +0000

    Building Matrix class

---
+++ Week-5/04/Matrix.java
@@ -0,0 +1,15 @@
+public class Matrix {
+  private int [] [] Arr;
+
+  public Matrix (int first, int second) {
+    Arr = new int[first][second];
+  } 
+
+  public setElement (int first, int second, int elem) {
+
+
+  }
+
+
+
+}
