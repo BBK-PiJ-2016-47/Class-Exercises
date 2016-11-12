d2f5f529c3baee7f22e007001191ee7a23e8527f
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-12 12:52:39 +0000

    Added method setElement

--- Week-5/04/Matrix.java
+++ Week-5/04/Matrix.java
@@ -6,10 +6,16 @@ public class Matrix {
   } 
 
   public setElement (int first, int second, int elem) {
-
-
+    if (first > Arr.length) {
+      System.out.println("Your first parameter is too large!")
+    } else if (second > Arr[0].length) {
+      System.out.println("Your second parameter is too large!")
+    } else {
+      Arr[first][second] = elem;
+    }
   }
 
+  public 
 
 
 }
