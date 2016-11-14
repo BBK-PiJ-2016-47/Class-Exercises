3cee8d3a3a772a425eb1381fc388dccb6efdc8b4
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-14 20:17:47 +0000

    Adding alt pointer

--- Week-7/01/SupermarketQueue.java
+++ Week-7/01/SupermarketQueue.java
@@ -1,4 +1,4 @@
-	public class SupermarketQueue implements PersonQueue {
+		public class SupermarketQueue implements PersonQueue {
 		private Person firstPerson = null
 		
 		public void insert(Person person) {
@@ -6,13 +6,18 @@
 			firstPerson = person;
 			return;
 		}
+		
+		person.nextPerson = firstPerson;
+		firstPerson = person;
+		}
+		/*
 	Person current = this.firstPerson;
 		while (current.getNextPerson() != null) {
 		current = current.getNextPerson();
 		}
 		current.setNextPerson(person);
 		}
-		
+		*/
 		public Person retrieve() {
 		if (firstPerson == null) {
 			System.out.println("There is nobody in the queue to serve!");
