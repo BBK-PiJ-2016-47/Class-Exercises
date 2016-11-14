bc9a0b7ae63a611f50ea8bb54204f3cc56f8d665
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-14 19:55:44 +0000

    Create SupermarketQueue.java

---
+++ Week-7/01/SupermarketQueue.java
@@ -0,0 +1,24 @@
+	public class SupermarketQueue implements PersonQueue {
+		private Person firstPerson = null
+		
+		public void insert(Person person) {
+		if (firstPerson == null) {
+			firstPerson = person;
+			return;
+		}
+	Person current = this.firstPerson;
+		while (current.getNextPerson() != null) {
+		current = current.getNextPerson();
+		}
+		current.setNextPerson(person);
+		}
+		
+		public Person retrieve() {
+		if (firstPerson == null) {
+			System.out.println("There is nobody in the queue to serve!");
+		}
+			firstPerson = firstPerson.getNextPerson();
+			return firstPerson;
+		}
+		}
+	
