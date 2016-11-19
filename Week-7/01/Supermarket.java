5dda49f145d171c48ba63c58d811572a3624f8ad
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-19 14:21:00 +0000

    Moved script into Supermarket.java

--- Week-7/01/Supermarket.java
+++ Week-7/01/Supermarket.java
@@ -1,6 +1,31 @@
 public class Supermarket  {
 	SupermarketQueue queue = new SupermarketQueue();
 	private int queueSize = 0;
+	private SupermarketQueue niceQueue = new SupermarketQueue();
+	
+		Person firstPerson = new Person("Bob", 12.50);
+		Person nextPerson = new Person("Jan", 19.0);
+		Person anotherPerson = new Person("Ralph", 14.63);
+		private Person head = null;
+	
+	public static void main(String[] args) {
+		PersonQueueScript script = new PersonQueueScript();
+		script.run();
+	}
+	public void run(){
+		System.out.println("Supermarket Queue!");
+		Supermarket mySupermarket = new Supermarket();
+		niceQueue.printQueue();
+		
+		System.out.println("Adding Bob");
+		mySupermarket.addPerson(firstPerson);
+		niceQueue.printQueue();
+		
+		System.out.println("Adding Jan");
+		mySupermarket.addPerson(nextPerson);
+		niceQueue.printQueue();
+		mySupermarket.servePerson();
+	}
 	
 	public void addPerson(Person person) {
 		queue.insert(person);
