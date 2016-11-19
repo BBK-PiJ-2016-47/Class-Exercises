a3d37eb3e071afbc73e370b18e3cbf8315fcfbc0
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-19 15:46:57 +0000

    Fix typo in main, add print line

--- Week-7/05/5.1/HashScript.java
+++ Week-7/05/5.1/HashScript.java
@@ -1,9 +1,10 @@
 public class HashScript {
-	public static void man(String[] args) {
+	public static void main(String[] args) {
 	System.out.println("Give me a string and I will calculate its hash code: ");
 	String str = System.console().readLine();
 	int hash = str.hashCode();
 	int smallHash = HashUtilities.shortHash(hash);
+	System.out.println("The hashcode for " + str + " is " + smallHash;
 	System.out.println("0 < " + smallHash + " < 1000");
 	}
 }
