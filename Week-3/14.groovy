7123876cf38827a9b9d23d1b8782e036ff02afab
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-19 21:58:56 +0000

    Fix infinite loop

--- Week-3/14.groovy
+++ Week-3/14.groovy
@@ -1,15 +1,22 @@
+/* Palindrome
+A palindrome is a word, phrase, number, or other sequence of units that may be read the same way in either
+direction. Examples of strict palindromes include “ABBA”, “madam”, “radar”, “kayak”, and “step on no pets”.
+Write a program that reads a text and detects whether the text is a strict palindrome.
+*/
+
 print "Please enter a word: "
 String input = System.console().readLine();
 
 int size = input.length() - 1;
 int i = 0;
 boolean isPalindrome = true;
-	while ((i < size) || (isPalindrome)) {
-		if (input[i] == input[size]) {
+	while (i < size) {
+		if (input.charAt(i) == input.charAt(size)) {
 			i++;
 			size--;
 		} else {
 			isPalindrome = false;
+			break;
 		}
 	}
 	if (isPalindrome) {
