7d795803192305ec6e4136e44fbe1b4e2206725c
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-19 14:18:19 +0000

    Adding prints for clarity

--- Week-7/01/Supermarket.java
+++ Week-7/01/Supermarket.java
@@ -4,11 +4,13 @@ public class Supermarket  {
 	
 	public void addPerson(Person person) {
 		queue.insert(person);
+		System.out.println(person.getName() + " is in the queue now.");
 		
 	}
 	
 	public void servePerson() {
 		queue.retrieve();
+		System.out.println(person.getName() + " has been served. They spent " + person.getShopCost());
 	}
 
 
