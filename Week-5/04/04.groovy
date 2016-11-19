f6dc2600c5d1b2bc09de7be9fbff6e3a6ead10bf
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-19 13:16:49 +0000

    Adding tests

--- Week-5/04/04.groovy
+++ Week-5/04/04.groovy
@@ -1,7 +1,11 @@
 Matrix firstTry = new Matrix(3,4);
 
 firstTry.setElement(1,2,3);
+firstTry.setElement(5,2,4);
+firstTry.setElement(2,6,1);
 
 
 String firstStr = firstTry.toString();
 println firstStr;
+
+firstTry.prettyPrint();
