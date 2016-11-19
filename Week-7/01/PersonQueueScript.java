d30e96c2fc4fc0fc4c94f8efd46a4b3fb3767772
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-19 14:00:13 +0000

    Move print method to SupermarketQueue

--- Week-7/01/PersonQueueScript.java
+++ Week-7/01/PersonQueueScript.java
@@ -1,8 +1,9 @@
 public class PersonQueueScript {
 	
-	Person firstPerson = new Person("Bob", 12.50);
+		Person firstPerson = new Person("Bob", 12.50);
 		Person nextPerson = new Person("Jan", 19.0);
 		Person anotherPerson = new Person("Ralph", 14.63);
+		private Person head = null;
 	
 	public static void main(String[] args) {
 		PersonQueueScript script = new PersonQueueScript();
@@ -10,33 +11,17 @@ public class PersonQueueScript {
 	}
 	public void run(){
 		System.out.println("Supermarket Queue!");
-		
-		
 		Supermarket mySupermarket = new Supermarket();
+		printQueue();
 		
+		System.out.println("Adding Bob");
+		mySupermarket.addPerson(firstPerson);
+		printQueue();
 		
+		System.out.println("Adding Jan");
+		mySupermarket.addPerson(nextPerson);
 		printQueue();
 	}
-	
-	public void printQueue() {
-		Person person = firstPerson;
-		boolean isFirst = true;
-		int count = 1; 
-		if (person == null) {
-			System.out.println("Nothing to print");
-			return;
-		}
-		do{
-			if (!isFirst) {
-				person = person.getNextPerson();
-			}
-			System.out.println("Person " + count);
-			System.out.println("Name   : " + person.getName());
-			System.out.println("Shop cost: " + person.getShopCost());
-			count++;
-			isFirst = false;
-		} while(person.getNextPerson() != null);
-	
-	
-}}
+
+}
 
