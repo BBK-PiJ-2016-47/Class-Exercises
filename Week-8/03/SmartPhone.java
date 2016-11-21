bb83f2612bb5535e549bb0f8f6c61d7ac769d4a8
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-21 19:59:19 +0000

    Create SmartPhone.java

---
+++ Week-8/03/SmartPhone.java
@@ -0,0 +1,24 @@
+public class SmartPhone extends MobilePhone {
+	public SmartPhone(String brand) {
+		super(brand);
+	}
+	public void browseWeb(String url) {
+		System.out.println("Opening webpage " + url);
+	}
+	
+	public String findPosition() {
+		System.out.println("You are here!");
+		return "You are here!";
+	}
+	
+	@Override
+	public void call(String number) {
+		String dialCode = number.substring(0,2);
+		if(dialCode.equals("00")) {
+			System.out.println("Calling " + number + " through the internet to save money");
+		} else {
+		super.call(number);
+		}
+		}
+	}
+	
