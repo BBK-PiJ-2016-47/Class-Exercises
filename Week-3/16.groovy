6a0a6bf8afff4f1aa5a264dd302968ab822bcd1d
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-11-21 12:25:40 +0000

    Create 16.groovy

---
+++ Week-3/16.groovy
@@ -0,0 +1,23 @@
+/*
+Text2number
+Write a program that reads a number with commas and decimal dots (such as 
+“23,419.34”) and then prints a number that is half of it. Do not use 
+Double.parseDouble(). If you were writing a simple spreadsheet, 
+you could use this code to parse the input in the cells.
+
+
+*/
+
+print "Please enter a number: "
+String num = System.console().readLine();
+int size = num.length();
+
+num = num.replace(",","");
+
+for (int i = 0; i < size; i++) {
+	if (num.charAt(i) == ) {
+  
+  }
+
+
+}
