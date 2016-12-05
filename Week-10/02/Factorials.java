97cee3ee1c1cd3f3971e7b14324b75c3b67f3be6
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-12-05 19:21:52 +0000

    Making variables private

--- Week-10/02/Factorials.java
+++ Week-10/02/Factorials.java
@@ -13,20 +13,20 @@ public static int recursiveFactorial(int n) {
 
 //iterative
 public static void iterativeFactorial (int n) {
-	int multiply = 1;
-	int originalNumber = n;
+	private int multiply = 1;
+	private int originalNumber = n;
+	
 	if (n == 1) {
 		System.out.println("The answer is 1");
-	}else {
+	} else {
 		while (n > 1) {
 			multiply = multiply * n;
 			n--;
 		}
 	System.out.println("The factorial of " + originalNumber + " is " + multiply);
-
 	}
-
 }
+	
 	public static void main(String [] args) {
 		iterativeFactorial(6);
 		recursiveFactorial(6);
