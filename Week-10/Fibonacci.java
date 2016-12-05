d77b0ccb932575c85466991615952201da7bc02d
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-12-05 19:19:54 +0000

    Create Fibonacci.java

---
+++ Week-10/Fibonacci.java
@@ -0,0 +1,39 @@
+public class Fibonacci {
+
+	public static int recursiveFibonacci(int n) {
+			if ((n == 1) || (n == 2)) {
+				return 1;
+			} else {
+				int result = recursiveFibonacci(n-1) + recursiveFibonacci(n-2); // method calls itself
+				return result;
+			}
+	}
+
+	public static int iterativeFibonacci(int n) {
+		if ((n==1) || (n==2)) {
+			System.out.println("The Fibonacci number at " + n + " is 1");
+			return 1;
+		} else {
+			int firstFib = 1;
+			int secondFib = 1;
+			int result = 0;
+				for(int x = 3; x <= n; x++) {
+					result = firstFib + secondFib;
+					firstFib = secondFib;
+					secondFib = result;
+				}
+			System.out.println("The Fibonacci number at " + n + " is " + result);
+			return result;
+		}
+	
+	}
+
+
+	public static void main(String [] args) {
+		recursiveFibonacci(2);
+		iterativeFibonacci(2);
+		recursiveFibonacci(7);
+		iterativeFibonacci(10);
+	
+	}
+}
