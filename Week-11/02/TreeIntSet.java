4b570be9121deb080fe85e1b8140968644705457
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-12-12 19:40:09 +0000

    Create TreeIntSet.java

---
+++ Week-11/02/TreeIntSet.java
@@ -0,0 +1,78 @@
+public class TreeIntSet implements IntSet {
+	int value;
+	TreeIntSet left;
+	TreeIntSet right;
+	
+	public TreeIntSet(int value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+	
+	public void add(int newNumber) {
+		if (newNumber > this.value) {
+			if (right == null) {
+				right = new TreeIntSet(newNumber);
+			} else {
+				right.add(newNumber);
+			}
+		} else {
+			if (left == null) {
+				left = new TreeIntSet(newNumber);
+			} else {
+				left.add(newNumber);
+			}
+		}
+	}
+	
+	public boolean contains(int n) {
+		if (n == this.value) {
+			System.out.println("The tree does contain " + n);
+			return true;
+		} else if (n > this.value) {
+			if (right == null) {
+				System.out.println("The tree does not contain " + n);
+				return false;
+			} else {
+				return right.contains(n);
+			}
+		} else {
+			if (left == null) {
+				System.out.println("The tree does not contain " + n);
+				return false;
+			} else {
+				return left.contains(n);
+			}
+		}
+		
+	}
+	
+	public boolean containsVerbose(int n) {
+		
+		if (n == this.value) {
+			System.out.println("Number checked: " + this.value);
+			System.out.println("The tree does contain " + n);
+			return true;
+		} else if (n > this.value) {
+			if (right == null) {
+				System.out.println("The tree does not contain " + n);
+				return false;
+			} else {
+				System.out.println("Number checked: " + this.value);
+				return right.contains(n);
+			}
+		} else {
+			if (left == null) {
+				System.out.println("The tree does not contain " + n);
+				return false;
+			} else {
+				System.out.println("Number checked: " + this.value);
+				return left.contains(n);
+			}
+		}
+		
+		
+		
+	}
+
+}
