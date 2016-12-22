e256a08374bd264f76fba4e0f2c3125226d1d45d
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-12-22 16:05:41 +0000

    Finishing methods but
    issues with backwards printing and removing

--- Week-6/03/HospitalLauncher03.java
+++ Week-6/03/HospitalLauncher03.java
@@ -1,13 +1,27 @@
 public class HospitalLauncher03 {
 	public static void main(String args[]) {
-		HospitalLauncher test = new HospitalLauncher();
+		HospitalLauncher03 test = new HospitalLauncher03();
 		test.run();	
 	}
 	
 	public void run() {
 	    System.out.println("===Hospital Manager===");
+		HospitalManager03 hospital = new HospitalManager03();
 		
 		Patient newPatient = new Patient("John",45,"flu");
-		addPatient(newPatient);
+		hospital.addPatient(newPatient);
+		newPatient = new Patient("Mary",55,"measles");
+		hospital.addPatient(newPatient);
+		newPatient = new Patient("Elliot",13,"ill");
+		hospital.addPatient(newPatient);
+		
+		hospital.printPatientList();
+	
+		
+		hospital.deletePatient("Mary");
+		hospital.deletePatient("Boris");
+		
+		hospital.printPatientList();
+		hospital.printPatientListBackwards();
 	}
 }
--- Week-6/03/HospitalManager03.java
+++ Week-6/03/HospitalManager03.java
@@ -1,6 +1,5 @@
 public class HospitalManager03 {
 	private Patient head = null; //first patient
-	private Patient tail = null;
 	private boolean finished = false;
 	private int count = 0;
 
@@ -9,6 +8,7 @@ public class HospitalManager03 {
 		//if this is the first patient
 		if (head == null) {
 			head = newPatient;
+			System.out.println("Patient has been added.");
 			return;
 		}
 		
@@ -20,42 +20,46 @@ public class HospitalManager03 {
 		current.setNextPatient(newPatient);
 		newPatient.setPreviousPatient(current);
 		count++;
+		System.out.println("Patient has been added.");
 	}
 	
  
 	public boolean deletePatient(String name) {
 		if (head == null) {
-		// list is empty, nothing to remove
 		return false;
-			}
-		if (head.getName().equals(name)) {
-			// first patient in the list must be removed
-			head = head.getNextPatient();
-			return true;
-			}
-		Patient current = head;
-		while (current.getNextPatient() != null) {
-			if (current.getNextPatient().getName().equals(name)) {
-			// We found it! It is the next one!
-			// Now link this patient to the one after the next
-			current.setNextPatient(current.getNextPatient().getNextPatient());
-			System.out.println("The patient has been deleted.");
-			return true;
-			}
-			current = current.getNextPatient();
-			count--;
 		}
-		System.out.println("No patient of that name to delete");
-		return false;
+	
+		Patient current = head;
+		while(current != null && !current.getName().equals(name)) {
+			current = current.getNextPatient();
+		}
+		if (current == null) {
+			return false;
+		}
+		if (current == head) {
+			head = current.getNextPatient();
+		}
+		if (current.getPreviousPatient() != null) {
+
+				current = current.getPreviousPatient();
+		current = current.getNextPatient();
+		current = current.getNextPatient();
+		}
+		if (current.getNextPatient() != null) {
+		current = current.getNextPatient();
+		current = current.getPreviousPatient();
+		current = current.getPreviousPatient();
+		}
+		return true;
 	}
 	
 	public void printPatientList() {
+		Patient patient = head;
 		if (patient == null) {
 			System.out.println("No patients in the hospital!");
 			return;
 		}
 			int counter = 1;
-			Patient patient = head;
 			boolean isFirst = true;
 		do{
 			if (!isFirst) {
@@ -73,7 +77,32 @@ public class HospitalManager03 {
 	}
 	
 	public void printPatientListBackwards() {
+		Patient patient = head;
+		if (patient == null) {
+		System.out.println("No patients in the hospital!");
+		return;
+		}
 		
+		int counter = 1;
+		boolean isFirst = true;
+
+		while(patient.getNextPatient() != null) {
+			patient = patient.getNextPatient();
+		}
+		
+
+		do {
+			if (!isFirst) {
+			patient = patient.getPreviousPatient();
+			}
+			
+			System.out.println("Patient " + counter);
+			System.out.println("Name   : " + patient.getName());
+			System.out.println("Illness: " + patient.getIllness());
+			System.out.println("Age    : " + patient.getAge());
+			counter++;
+			isFirst = false;
+		} while (patient != null);
 	}
 	
 	public int patientCount() {
