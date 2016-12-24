e8d3606b8a9ed849fd4df02d7f67147df20dc163
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2016-12-24 15:10:53 +0000

    Add 4.1

---
+++ Week-8/04/4.1/MobilePhone.java
@@ -0,0 +1,35 @@
+public class MobilePhone extends OldPhone {
+	private int called = 1;
+	private String[] lastNumbersCalled = new String[10];
+	@Override
+	public void call(String number){
+		super.call(number);
+		if (called <= 10) {
+			lastNumbersCalled[called-1] = number;
+			called++;
+		} else {
+			called = 1;
+			lastNumbersCalled[called-1] = number;
+			
+		}
+		
+		
+	}
+
+	public void ringAlarm(String alarm){
+		System.out.println("The alarm is set for " + alarm);
+	}
+
+	private void playGame(String game) {
+		System.out.println("Opening game: " + game);
+	}
+
+	public void printLastNumbers() {
+		for (int i = 0; i<10; i++) {
+			if (lastNumbersCalled[i] != null){
+			System.out.println(lastNumbersCalled[called - i]);
+			}
+		}
+		
+	}
+}
---
+++ Week-8/04/4.1/OldPhone.java
@@ -0,0 +1,7 @@
+public class OldPhone {
+	
+	void call (String number) {
+	System.out.println("Calling " + number);
+	}
+
+}
