41f59d5ae2e2a05088bb681a7cb2ba05c4d7aa84
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2017-01-14 14:19:59 +0000

    Create Stack.java

---
+++ Week-6/06/Stack.java
@@ -0,0 +1,18 @@
+public class Stack {
+
+	public void empty() {
+	  System.out.println("Stack is empty.");	
+	}
+	
+	public void push(int x) {
+		System.out.println("Pushing " + x + "...");
+	}
+	
+	public void pop() {
+		int x = element;
+		System.out.println("Popping... it's a " + x);
+	}
+	
+	
+
+}
