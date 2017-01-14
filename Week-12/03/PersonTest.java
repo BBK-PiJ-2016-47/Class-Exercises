d2d2106feea84831f4483a2ee4a1a680ae944c23
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2017-01-14 17:37:33 +0000

    adding test for two spaces

---
+++ Week-12/03/PersonTest.java
@@ -0,0 +1,21 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+public class PersonTest {
+	@Test
+	public void testsNormalName() {
+		Person p = new Person();
+		String input = "Dereck Robert Yssirt";
+		String output = p.getInitials(input);
+		String expected = "DRY";
+		assertEquals(output, expected);
+	}
+	
+	@Test
+	public void testsNameWithTwoSpaces() {
+		Person p = new Person();
+		String input = "Dereck  Robert Yssirt";
+		String output = p.getInitials(input);
+		String expected = "DRY";
+		assertEquals(output, expected);	
+	}
+}
