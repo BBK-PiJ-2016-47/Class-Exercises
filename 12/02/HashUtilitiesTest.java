254cb4bab779b54d807fc96e68f946ab09aed01f
BBK-PiJ-2016-47 <hspenc03@dcs.bbk.ac.uk>
2017-01-14 17:12:45 +0000

    Update methods to check range

--- 12/02/HashUtilitiesTest.java
+++ 12/02/HashUtilitiesTest.java
@@ -1,14 +1,22 @@
-import org.junit.Test;
-import org.junit.Assert;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class HashUtilitiesTest {
-	
+
 	@Test
-	public void hashCodeRangeTest {
+	public void hashHighRange() {
 		HashUtilities utility = new HashUtilities();
 		for (int i = 0; i < 2000; i++) {
 			int hash = utility.shortHash(i);
-			assertTrue(hash > 0 && hash < 1000);
+			assertTrue(hash < 1000);
+		}
+	}
+	
+	public void hashLowRange() {
+		HashUtilities utility = new HashUtilities();
+		for (int i = 0; i < 2000; i++) {
+			int hash = utility.shortHash(i);
+			assertTrue(hash > 0);
 		}
 	}
 
