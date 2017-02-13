850a4068eb69195cf55da6951e226cad9d180644
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-02-13 18:35:58 +0000

    Create FilePrint

---
+++ Week-17/01/FilePrint.java
@@ -0,0 +1,24 @@
+/**
+ *Write a program that shows on screen the names of the files in the current directory. (Hint: look at methods from
+ *class File).
+ */
+ import java.io.File;
+ 
+ public class FilePrint {
+	 
+	 public void printFiles(String filename) {
+	 File testDirectory = new File(filename);
+
+	 File[] fileArray = testDirectory.listFiles();
+	   for(int i = 0; i < fileArray.length; i++) {
+		   System.out.println(fileArray[i]);
+	   }
+	 }
+	 public static void main(String[] args) {
+		 FilePrint tester = new FilePrint();
+		 tester.printFiles("C:\\Users\\hanna\\Documents\\GitHub\\Class-Exercises");
+		 
+	 }
+
+	 
+ }
