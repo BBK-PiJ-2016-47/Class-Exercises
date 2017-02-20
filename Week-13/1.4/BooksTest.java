39e85194aec200b4f0b5586e94ccc62395353520
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-02-20 19:38:17 +0000

    Add BooksTest

--- Week-13/1.4/BooksTest.java
+++ Week-13/1.4/BooksTest.java
@@ -1,17 +1,18 @@
 import org.junit.*; 
-import static org.junit.Assert.*;
+import org.junit.Assert.*;
+
 
 public class BooksTest {
 
-	Books newBook = new Books("Narnia", "Lewis");
-	
+	Book newBook = new BookImpl("Narnia", "Lewis");
+
 	@Test
 	public void testsGetTitle() {
 		String output = newBook.getTitle();
 		String expected = "Narnia";
 		assertEquals(expected, output);
 	}
-	
+
 	@Test
 	public void testsGetAuthor() {
 		String output = newBook.getAuthor();
