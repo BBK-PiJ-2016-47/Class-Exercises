5b88c3b3ba22ec18e3c87287ee83aa027aec3d35
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-02-24 19:12:32 +0000

    Create circularlist

---
+++ Week-6/04/CircularList.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package circularlist;
+
+/**
+ *
+ * @author spencerh
+ */
+public class CircularList {
+
+        private Patient head = null; //first patient
+        private Patient current = null; //traverser
+	private boolean repeating = false; //is true when head
+	private int count = 0;
+
+        public void addPatient(Patient newPatient) {
+            if (head == null) {
+		head = newPatient;
+                newPatient.setNextPatient(newPatient);
+		System.out.println("Patient " + newPatient.getName() + " has been added");
+                count++;
+                newPatient.setId(count);
+		return;
+            }
+
+            current = this.head;
+            while(current.getNextPatient().getId() != 1) {
+                current = current.getNextPatient();
+            }
+            current.setNextPatient(newPatient);
+            current.getNextPatient().setNextPatient(head);
+
+            count++;
+            System.out.println("Patient " + newPatient.getName() + " has been added");
+            newPatient.setId(count);
+        }
+
+        public void deletePatient(String name){
+            if (head == null) {
+                System.out.println("There are no patients to delete!");
+                return;
+            }
+           //if first person
+            current = this.head;
+            if (current.getName().equals(name)) {
+                head = head.getNextPatient();
+                head.setId(1);
+                System.out.println(name + " has been deleted");
+                return;
+            }
+
+            while(current.getNextPatient().getId() != 1  && current.getNextPatient().getName() != name) {
+                current = current.getNextPatient();
+            }
+
+            if (current.getNextPatient().getId() == 1) {
+                System.out.println(name + " wasn't found!");
+                return;
+            }
+         
+                current.setNextPatient(current.getNextPatient().getNextPatient());
+                System.out.println(name + " has been deleted");
+            
+        }
+
+
+        public void printPatientList(){
+            if (head == null) {
+		System.out.println("No patients in the hospital!");
+                return;
+            }
+
+            current = this.head;
+            do {
+                System.out.println("ID     : " + current.getId());
+                System.out.println("Name   : " + current.getName());
+		System.out.println("Illness: " + current.getIllness());
+		System.out.println("Age    : " + current.getAge());
+                current = current.getNextPatient();
+
+            } while(current.getId() != 1) ;
+        }
+        
+
+}
---
+++ Week-6/04/Patient.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package circularlist;
+
+/**
+ *
+ * @author spencerh
+ */
+public class Patient {
+    private int id;
+    private String name;
+    private int age;
+    private String illness;
+    private Patient nextPatient;
+    private Patient prevPatient;
+
+    public Patient(String name, int age, String illness) { //constructor
+        this.id = 0;
+            this.name = name;
+            this.age = age;
+            this.illness = illness;
+            this.nextPatient = null; // Pointer to another patient!
+	}
+
+        public int getId(){
+            return this.id;
+	}
+
+        public void setId(int id){
+            this.id = id;
+	}
+
+	public Patient getNextPatient(){
+		return this.nextPatient;
+	}
+
+	public Patient getPreviousPatient() {
+		return this.prevPatient;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+	public int getAge(){
+		return this.age;
+	}
+	public String getIllness(){
+		return this.illness;
+	}
+	public void setNextPatient(Patient patient){
+		this.nextPatient = patient;
+	}
+}
