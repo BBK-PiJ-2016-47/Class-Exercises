ec975ab48f380728a1f50e32b6df2986ed225198
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-04-15 11:21:03 +0100

    Create input method to avoid repetition, removed magic numbers

--- Week-14/03/3b/ImprovedAverage.java
+++ Week-14/03/3b/ImprovedAverage.java
@@ -5,44 +5,46 @@ public class ImprovedAverage {
     testing.getAverage();	
   }
   int [] avgArr;
-  public int getAverage() {
-  System.out.println("How many numbers would you like to average?");
-  String sSize = System.console().readLine();
-  try {
-  int Size = Integer.parseInt(sSize);
-  avgArr = new int[Size];
-  } catch (NumberFormatException ex){
-	  System.out.println("That's not a number! Please restart.");
-	  return 0;
-  }
-  
-
   int total = 0;
-    try {
-      for(int i = 0; i < 10; i++) {
-        System.out.println("Please enter a number: ");
+  int size = 0;
+  
+  private int getInput(int i) {
+	  System.out.println("Please enter a number: ");
         String sNum = System.console().readLine();
         int num = Integer.parseInt(sNum);
         avgArr[i] = num;
         total += num;
+		return total;
+  }
+  
+  public int getAverage() {
+  System.out.println("How many numbers would you like to average?");
+  String sSize = System.console().readLine();
+  try {
+  size = Integer.parseInt(sSize);
+  avgArr = new int[size];
+  } catch (NumberFormatException ex){
+	  System.out.println("That's not a number! Please restart.");
+	  return 0;
+  }
+
+    try {
+      for(int i = 0; i < size; i++) {
+        getInput(i);
       }
     } catch (NumberFormatException ex) {
       System.out.println("That wasn't a number, please try again!");
 	  int i = 0;
-	  while( i < 10) {
+	  while( i < size) {
         if (avgArr[i] == 0) {
-	      System.out.println("Please enter a number: ");
-          String sNum = System.console().readLine();
-          int num = Integer.parseInt(sNum);
-          avgArr[i] = num;
-          total += num;
+	      getInput(i);
 		  i++;
 		} else {
 			i++;
 		}
 	  }
     }
-    int result = (total / 10);
+    int result = (total / size);
     System.out.println("The average is: " + result);
     return result;
   }
