0b0b1c44278e6dadf641164d49be2054bfd929f9
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-04-15 11:21:25 +0100

    added parameter for negative age

--- Week-14/04/Patient.java
+++ Week-14/04/Patient.java
@@ -1,27 +1,26 @@
 public class Patient {
-  String name;
-  int birthYear;
-  
-  public Patient(String name, int birthYear) {
-    this.name = name;
-	if (birthYear < 1886) {
-	this.birthYear = birthYear;
-	} else {
-	  throw new IllegalArgumentException();
-	}
-  }
-
-  public void createNewPatientRecord() {
-    System.out.println("Please enter patient name");
-	String pName = System.console().readLine();
-	System.out.println("Please enter patient's year of birth");
-	String sYear = System.console().readLine();
-	int year = Integer.parseInt(sYear);
- 
-    new Patient(pName, year);
-  }
-
-  public static void main(String[] args) {
+	  String name;
+	  int birthYear;
 	  
-  }
-}
+	  public Patient(String name, int birthYear) {
+	    this.name = name;
+		if (birthYear > 1886 && birthYear < 2017) {
+		this.birthYear = birthYear;
+		} else {
+		  throw new IllegalArgumentException();
+		}
+	  }
+
+	  public static void createNewPatientRecord() {
+	    System.out.println("Please enter patient name");
+		String pName = System.console().readLine();
+		System.out.println("Please enter patient's year of birth");
+		String sYear = System.console().readLine();
+		int year = Integer.parseInt(sYear);
+	    new Patient(pName, year);
+	  }
+
+	  public static void main(String[] args) {
+		  Patient.createNewPatientRecord();
+	  }
+	}
