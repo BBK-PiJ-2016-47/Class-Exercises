6617523486438e2b30b69950fb19646a06531436
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-04-16 22:19:55 +0100

    Made method print list

--- Week-15/03/Hailstone.java
+++ Week-15/03/Hailstone.java
@@ -1,28 +1,29 @@
 import java.util.ArrayList;
 
 public class Hailstone {
-  ArrayList<Integer> hailArr = new ArrayList<Integer>;
+  ArrayList<Integer> hailArr = new ArrayList<Integer>();
   int ans = 0;
   public int hailMethod(int n) {
 	if (n == 1) {
-	  System.out.println("n has converged to 1: ");
+	  System.out.print("n has converged to 1: ");
 	  return 1;
 	}
-	while(n != 1) {//can do a terrnary operator here - much simpler
+		
+	while(n != 1) {
       if (n%2==0) {
 	    System.out.println("Even number: " + n);
 		hailArr.add(n);
 	    ans =  hailMethod(n/2);
 	    return ans;
 	  } else {
-	    System.out.println("Odd number: " + n);
+	    System.out.println(" Odd number: " + n);
 	    int result = (3*n)+1;
 		hailArr.add(n);
 	    ans = hailMethod(result);
 	    return ans;
 	  }
 	}
-	return 0;//want it to print a list of integers
+	return 0;
   }
   
   public static void main(String[] args) {
